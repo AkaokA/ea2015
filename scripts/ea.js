0e9e11affcf4312fc076147f2b674f2c976da57d
Eric Akaoka <eric.akaoka@gmail.com>
2015-09-29 14:24:07 -0400

    Big update, mostly photo gallery stuff.

--- scripts/ea.js
+++ scripts/ea.js
@@ -1,3 +1,4 @@
+var $html;
 var $body;
 var bodyScrollHeight;
 
@@ -7,19 +8,31 @@ var $colorElements;
 var $strokeColorElements;
 var $fillColorElements;
 
+var $photoGallery;
+var $fullscreenPhotoWrapper;
+var $fullscreenPhoto;
+var $fullscreenPhotoInfo;
 
 function registerElements(){
+	$html = $("html");
 	$body = $("body");
+	
 	bodyScrollHeight = $("body")[0].scrollHeight;
+	
 	$bgcolorElements = $("h2");
 	$colorElements = $("body, a");
 	$strokeColorElements = $("svg.stroke path");
 	$fillColorElements = $("svg.fill path");
+	
 	$homepageIntroDiv = $("#homepage-intro");
+	
+	$photoGallery = $("#photo-gallery");
+	$fullscreenPhotoWrapper = $(".fullscreen-photo-wrapper");
+	$fullscreenPhoto = $(".fullscreen-photo");
+	$fullscreenPhotoInfo = $(".fullscreen-photo-info");
 }
 
 var baseColor = jQuery.Color( "#222222" );
-var otherColor = jQuery.Color( "#447088" );
 
 function setGlobalColor(color){
 	$bgcolorElements.css("background-color", color.toRgbaString() );
@@ -28,21 +41,68 @@ function setGlobalColor(color){
 	$fillColorElements.attr("fill", color.toHexString() );
 }
 
-function fivehundredpx() {
-	// initialize 500px SDK
-	_500px.init({
-	  sdk_key: '92d730ffbdca5625f0ded59fb58648fb840133f1'
+var pageToLoad = 1;
+var imagesPerPage = 12;
+
+function load500pxPhotos() {
+	// get my photos
+  _500px.api('/photos', {feature: 'user', username: 'akaoka', image_size: 21, page: pageToLoad, rpp: imagesPerPage}, function (response) {
+	  	  
+		$.each(response.data.photos, function () {			
+			$photoGallery.append('<div class="thumbnail" style="background-image: url(' + this.images[0].url + ')" data="'+ this.id +'"></div>');
+			
+		});
+	});
+		
+	pageToLoad++;	
+}
+
+var photoIsFullscreen = false;
+
+function photoGalleryEvents() {
+	$photoGallery.on("click", ".thumbnail", function(){
+		photoIsFullscreen = true;
+		
+		// prevent scroll on body
+		$html.css("overflow", "hidden");
+		
+		_this = $(this);
+		var photoId = $(this).attr("data");
+		
+		$fullscreenPhotoWrapper.fadeIn( function(){
+			
+			// get photo by id
+		  _500px.api('/photos/'+ photoId, {image_size: 2048}, function (response) {
+				var imageUrl = response.data.photo.images[0].url;
+				var photoTitle = response.data.photo.name;
+				
+				$fullscreenPhoto.load(imageUrl, function(){
+					console.log("lol");
+					$fullscreenPhoto.css("background-image", "url(" + imageUrl + ")").fadeIn();
+					$fullscreenPhotoInfo.html('<p>'+ photoTitle +'</p>');
+				});
+				
+			});			
+		});
 	});
 	
-	// get my photos
-  _500px.api('/photos', {feature: 'user', username: 'akaoka', image_size: 21, page: 1, rpp: 18}, function (response) {
-	  	  
-		$.each(response.data.photos, function () {
-			console.log(this);
-			
-			$('#photo-gallery').append('<a href="https://500px.com' + this.url + '"><div class="thumbnail" style="background-image: url(' + this.images[0].url + ')"></div></a>');
+	$fullscreenPhotoWrapper.click(function(){
+		$(this).fadeOut( function(){
+			$fullscreenPhoto.css("background-image", "none");
+			$fullscreenPhotoInfo.empty();
+			$html.css("overflow", "");
+		});
+		
+		photoIsFullscreen = false;
+	});
+}
 
-		});  });
+function scrollEvents() {		
+	// transitions on scroll
+	$( window ).scroll(function(event) {
+			// intro section parallax
+			$homepageIntroDiv.css( "-webkit-transform", "translateY("+ $body.scrollTop()/3 +"px)" );		
+	});
 }
 
 $( document ).ready(function() {
@@ -50,16 +110,26 @@ $( document ).ready(function() {
 	registerElements();
 	setGlobalColor(baseColor);
 	
-	fivehundredpx();
-	
-	var homepageIntroHeight = $homepageIntroDiv.height();
-	var homepageIntroMinHeight = 280;
-		
-	// transitions on scroll
-	$( window ).scroll(function() {
-		
-		// intro section parallax
-		$homepageIntroDiv.css( "-webkit-transform", "translateY("+ $body.scrollTop()/3 +"px)" );
-		
+	// initialize 500px SDK
+	_500px.init({
+	  sdk_key: '92d730ffbdca5625f0ded59fb58648fb840133f1'
 	});
+	
+	// load 500px photos
+	load500pxPhotos();
+	$(".load-more-photos").click(function(event){
+		event.preventDefault();
+		load500pxPhotos();
+	});
+	
+	photoGalleryEvents();
+	
+	scrollEvents();
+	
+	$(document).bind('touchmove', function(e){
+		if (photoIsFullscreen) {
+			e.preventDefault();           
+		}
+	});
+	
 });
